a719af49ded0d1ba73bde5f42b882888b84ccde3
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-11-09 14:11:10 +0100

    target module by selecting all elements

--- Gallery-Project/app.js
+++ Gallery-Project/app.js
@@ -14,9 +14,15 @@ function getElementSelected(selection) {
 // Creating constructor function
 
 function Gallery(element) {
-	console.log(element);
+  this.list = [...element.querySelectorAll('.img')]; //selecting all images as an array 
+	// target module by selecting all elements
+	this.modal = getElementSelected('.modal');
+	this.modalImage = getElementSelected('.main-img');
+	this.modalImages = getElementSelected('.modal-images');
+	this.closeBtn = getElementSelected('.close-btn');
+	this.nextBtn = getElementSelected('.next-btn');
+	this.prevBtn = getElementSelected('.prev-btn');
 }
 
 const nature = new Gallery(getElementSelected('.nature'));
 const city = new Gallery(getElementSelected('.city'));
-
