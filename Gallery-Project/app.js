54d872d8892fb14a36f2d46c5f8cd877420224d8
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-11-09 16:12:38 +0100

    add EventListener and remove to close btn

--- Gallery-Project/app.js
+++ Gallery-Project/app.js
@@ -26,6 +26,9 @@ function Gallery(element) {
   this.prevBtn = getElementSelected('.prev-btn');
   // bind functions
   //this.openModal = this.openModal.bind(this);
+  this.closeModal = this.closeModal.bind(this);
+  this.nextImage = this.nextImage.bind(this);
+  this.prevImage = this.prevImage.bind(this);
   //container event
   this.container.addEventListener(
     'click',
@@ -41,7 +44,7 @@ function Gallery(element) {
 Gallery.prototype.openModal = function (selectedImage, list) {
   //console.log(this);
   this.setMainImage(selectedImage);
-	//displaying list of foto in open modal
+  //displaying list of foto in open modal
   this.modalImages.innerHTML = list
     .map(function (image) {
       return `<img src="${
@@ -50,6 +53,9 @@ Gallery.prototype.openModal = function (selectedImage, list) {
     })
     .join('');
   this.modal.classList.add('open');
+  this.closeBtn.addEventListener('click', this.closeModal);
+  this.nextBtnBtn.addEventListener('click', this.nextImage);
+  this.prevBtn.addEventListener('click', this.prevImage);
 };
 
 Gallery.prototype.setMainImage = function (selectedImage) {
@@ -57,5 +63,14 @@ Gallery.prototype.setMainImage = function (selectedImage) {
   this.imageName.textContent = selectedImage.title;
 };
 
+Gallery.prototype.closeModal = function () {
+  this.modal.classList.remove('open');
+  this.closeBtn.removeEventListener('click', this.closeModal);
+  this.nextBtnBtn.removeEventListener('click', this.nextImage);
+  this.prevBtn.removeEventListener('click', this.prevImage);
+};
+
+Gallery.prototype.nextImage = function () {};
+Gallery.prototype.prevImage = function () {};
 const nature = new Gallery(getElementSelected('.nature'));
 const city = new Gallery(getElementSelected('.city'));
