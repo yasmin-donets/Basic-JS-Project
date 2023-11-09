2ff9062250ff4e74bc0ea32d3a194cd31189aca1
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-11-09 14:29:30 +0100

    open modul setUp

--- Gallery-Project/app.js
+++ Gallery-Project/app.js
@@ -14,15 +14,28 @@ function getElementSelected(selection) {
 // Creating constructor function
 
 function Gallery(element) {
-  this.list = [...element.querySelectorAll('.img')]; //selecting all images as an array 
-	// target module by selecting all elements
-	this.modal = getElementSelected('.modal');
-	this.modalImage = getElementSelected('.main-img');
-	this.modalImages = getElementSelected('.modal-images');
-	this.closeBtn = getElementSelected('.close-btn');
-	this.nextBtn = getElementSelected('.next-btn');
-	this.prevBtn = getElementSelected('.prev-btn');
+  this.container = element;
+  this.list = [...element.querySelectorAll('.img')]; //selecting all images as an array
+  // target module by selecting all elements
+  this.modal = getElementSelected('.modal');
+  this.modalImage = getElementSelected('.main-img');
+  this.modalImages = getElementSelected('.modal-images');
+  this.closeBtn = getElementSelected('.close-btn');
+  this.nextBtn = getElementSelected('.next-btn');
+  this.prevBtn = getElementSelected('.prev-btn');
+  // bind functions
+  //this.openModal = this.openModal.bind(this);
+	//container event
+  this.container.addEventListener('click', function (e) {
+    this.openModal();
+  }.bind(this));
 }
+
+Gallery.prototype.openModal = function () {
+  console.log(this);
+  //console.log('open modal');
+  this.modal.classList.add('open');
+};
 
 const nature = new Gallery(getElementSelected('.nature'));
 const city = new Gallery(getElementSelected('.city'));
