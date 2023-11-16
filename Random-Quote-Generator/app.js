69c06e459160e2697ec320b201a6a5fee32d9440
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-11-16 21:26:37 +0100

    Basic XMLHttpRequest - 1

--- Random-Quote-Generator/app.js
+++ Random-Quote-Generator/app.js
@@ -1,4 +1,4 @@
-// selecting elements 
+// selecting elements
 const quoteElement = document.querySelector('.quote');
 const authorElement = document.querySelector('.author');
 const btnElement = document.querySelector('#btn');
@@ -8,6 +8,28 @@ const URL = 'https://api.quotable.io/random';
 
 // add Event Listener
 
-btnElement.addEventListener('click',()=>{
-	
-})
+btnElement.addEventListener('click', () => {
+  getData(URL);
+});
+
+// get Data function
+function getData(url) {
+  const xhr = new XMLHttpRequest(); //readyState:0
+  xhr.open('GET', url); //readyState:1
+  xhr.send(); //readyState:4
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== 4) return;
+    if (xhr.status === 200) {
+      const { content: quote, author: quoteAuthor } = JSON.parse(
+        xhr.responseText
+      );
+      quoteElement.textContent = quote;
+			authorElement.textContent = quoteAuthor;
+    } else {
+      console.log({
+        status: xhr.status,
+        text: xhr.statusText,
+      });
+    }
+  };
+}
