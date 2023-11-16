8fcb07b41b9e0191942af1fd79cc43a226da68be
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-11-16 21:42:54 +0100

    Fetch JS - 3

--- Random-Quote-Generator/app.js
+++ Random-Quote-Generator/app.js
@@ -9,34 +9,13 @@ const URL = 'https://api.quotable.io/random';
 // add Event Listener
 
 btnElement.addEventListener('click', () => {
-  getData(URL)
+  fetch(URL)
+    .then((data) => data.json())
     .then((response) => displayData(response))
     .catch((err) => console.log(err));
 });
 
-// get Data function
-function getData(url) {
-  //returning new promise
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.send();
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState !== 4) return;
-      if (xhr.status === 200) {
-        resolve(xhr.responseText);
-      } else {
-        reject({
-          status: xhr.status,
-          text: xhr.statusText,
-        });
-      }
-    };
-  });
-}
-
-function displayData(data) {
-  const { content: quote, author: quoteAuthor } = JSON.parse(data);
+function displayData({ content: quote, author: quoteAuthor }) {
   quoteElement.textContent = quote;
   authorElement.textContent = quoteAuthor;
 }
--- Random-Quote-Generator/utils/basic-1.js
+++ Random-Quote-Generator/utils/basic-1.js
@@ -33,3 +33,51 @@ function getData(url) {
     }
   };
 }
+
+
+
+/// SECOND 
+
+
+// selecting elements
+const quoteElement = document.querySelector('.quote');
+const authorElement = document.querySelector('.author');
+const btnElement = document.querySelector('#btn');
+
+// URL
+const URL = 'https://api.quotable.io/random';
+
+// add Event Listener
+
+btnElement.addEventListener('click', () => {
+  getData(URL)
+    .then((response) => displayData(response))
+    .catch((err) => console.log(err));
+});
+
+// get Data function
+function getData(url) {
+  //returning new promise
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== 4) return;
+      if (xhr.status === 200) {
+        resolve(xhr.responseText);
+      } else {
+        reject({
+          status: xhr.status,
+          text: xhr.statusText,
+        });
+      }
+    };
+  });
+}
+
+function displayData(data) {
+  const { content: quote, author: quoteAuthor } = JSON.parse(data);
+  quoteElement.textContent = quote;
+  authorElement.textContent = quoteAuthor;
+}
