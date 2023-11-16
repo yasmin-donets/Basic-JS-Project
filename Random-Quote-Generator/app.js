5730ee5a74da107ab2ee3c9938b53c3454bfc23a
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-11-16 21:37:09 +0100

    basic-js-promise-2

--- Random-Quote-Generator/app.js
+++ Random-Quote-Generator/app.js
@@ -9,27 +9,34 @@ const URL = 'https://api.quotable.io/random';
 // add Event Listener
 
 btnElement.addEventListener('click', () => {
-  getData(URL);
+  getData(URL)
+    .then((response) => displayData(response))
+    .catch((err) => console.log(err));
 });
 
 // get Data function
 function getData(url) {
-  const xhr = new XMLHttpRequest(); //readyState:0
-  xhr.open('GET', url); //readyState:1
-  xhr.send(); //readyState:4
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== 4) return;
-    if (xhr.status === 200) {
-      const { content: quote, author: quoteAuthor } = JSON.parse(
-        xhr.responseText
-      );
-      quoteElement.textContent = quote;
-			authorElement.textContent = quoteAuthor;
-    } else {
-      console.log({
-        status: xhr.status,
-        text: xhr.statusText,
-      });
-    }
-  };
+  //returning new promise
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== 4) return;
+      if (xhr.status === 200) {
+        resolve(xhr.responseText);
+      } else {
+        reject({
+          status: xhr.status,
+          text: xhr.statusText,
+        });
+      }
+    };
+  });
+}
+
+function displayData(data) {
+  const { content: quote, author: quoteAuthor } = JSON.parse(data);
+  quoteElement.textContent = quote;
+  authorElement.textContent = quoteAuthor;
 }
