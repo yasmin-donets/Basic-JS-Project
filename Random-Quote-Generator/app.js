31f1f3641b7ca1db6fc9a487f05e2debfbd6bb7d
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-11-16 21:05:36 +0100

    add Event Listener

--- Random-Quote-Generator/app.js
+++ Random-Quote-Generator/app.js
@@ -2,5 +2,12 @@
 const quoteElement = document.querySelector('.quote');
 const authorElement = document.querySelector('.author');
 const btnElement = document.querySelector('#btn');
+
 // URL
 const URL = 'https://api.quotable.io/random';
+
+// add Event Listener
+
+btnElement.addEventListener('click',()=>{
+	
+})
