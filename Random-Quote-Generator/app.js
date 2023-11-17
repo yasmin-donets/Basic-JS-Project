ca8cd3d40dac1517d6816c62f50a2ebcb80350bf
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-11-17 20:40:13 +0100

    final

--- Random-Quote-Generator/app.js
+++ Random-Quote-Generator/app.js
@@ -2,7 +2,7 @@
 const quoteElement = document.querySelector('.quote');
 const authorElement = document.querySelector('.author');
 const btnElement = document.querySelector('#btn');
-
+const copyBtn = document.querySelector('.feature');
 // URL
 const URL = 'https://api.quotable.io/random';
 
@@ -14,9 +14,15 @@ btnElement.addEventListener('click', async () => {
     const response = await data.json();
     displayData(response);
   } catch (error) {
-		console.log(error);
-	}
+    console.log(error);
+  }
 });
+
+// Copy function 
+copyBtn.addEventListener('click', () => {
+  navigator.clipboard.writeText(quoteElement.innerText);
+});
+
 
 function displayData({ content: quote, author: quoteAuthor }) {
   quoteElement.textContent = quote;
