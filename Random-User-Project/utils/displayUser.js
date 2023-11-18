d2bb0da452d9cdfb59a22dbbb5b5e04bcbbd8c17
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-11-18 18:11:53 +0100

    removeActive

--- Random-User-Project/utils/displayUser.js
+++ Random-User-Project/utils/displayUser.js
@@ -1,5 +1,5 @@
-import get from "./getElement.js";
-
+import get from './getElement.js';
+import removeActive from './removeActive.js';
 //selecting elements
 const imgEl = get('.user-img');
 const userTitle = get('.user-title');
@@ -11,12 +11,15 @@ const displayUser = (person) => {
   userValue.textContent = person.name;
   userTitle.textContent = `My name is`;
   // remove and add class active
-
+  removeActive(btns);
+  btns[0].classList.remove('active');
   btns.forEach((btn) => {
     const label = btn.dataset.label;
     btn.addEventListener('click', () => {
       userTitle.textContent = `My ${label} is:`;
       userValue.textContent = person[label];
+      removeActive(btns);
+      btn.classList.add('active');
     });
   });
 };
