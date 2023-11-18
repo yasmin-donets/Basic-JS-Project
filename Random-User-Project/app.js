8cdcba19a7a8a5ca69eb9b36d32e76f92cd5495d
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-11-18 18:02:09 +0100

    displayUser function

--- Random-User-Project/app.js
+++ Random-User-Project/app.js
@@ -2,25 +2,34 @@
 import get from './utils/getElement.js';
 import getUser from './utils/fetchUser.js';
 //selection all elements needed
-const imgEl = get('.user-img');
 const btnEl = get('.btn');
+const imgEl = get('.user-img');
 const userTitle = get('.user-title');
 const userValue = get('.user-value');
 const valuesList = get('.values-list');
-
+const btns = [...document.querySelectorAll('.icon')];
 
 //main
-const showUser = async() => {
+const showUser = async () => {
   const person = await getUser();
-	console.log(person);
+  displayUser(person);
 };
 
+const displayUser = (person) => {
+  imgEl.src = person.image;
+  userValue.textContent = person.name;
+  userTitle.textContent = `My name is`;
+  // remove and add class active
 
-
-const displayUser = () =>{
-
-}
-
+  btns.forEach((btn) => {
+    const label = btn.dataset.label;
+    btn.addEventListener('click', () => {
+      userTitle.textContent = `My ${label} is:`;
+      userValue.textContent = person[label];
+			console.log(person[label]);
+    });
+  });
+};
 
 // adding event listeners
 window.addEventListener('DOMContentLoaded', showUser);
