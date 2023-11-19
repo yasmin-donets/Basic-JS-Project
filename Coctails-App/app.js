b873535d92c4d92ee5b139e8d628e2cb8bbf35ea
yasmindonets17 <102476760+yasmindonets17@users.noreply.github.com>
2023-11-19 21:12:48 +0100

    fetch drinks from Api

--- Coctails-App/app.js
+++ Coctails-App/app.js
@@ -1,1 +1,7 @@
-const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a';
+import fetchDrinks from './utils/fetchDrinks.js';
+
+const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=b';
+
+window.addEventListener('DOMContentLoaded', () => {
+  fetchDrinks(URL);
+});
